d16cfd8cc5bd34f27b9767ea13edb8dd8a67e7c6
emina <emina.sirbubalo@stu.ibu.edu.ba>
2022-05-26 00:08:29 +0200

    home fragment updates

--- app/src/main/java/com/example/soundwave/HomeFragment.java
+++ app/src/main/java/com/example/soundwave/HomeFragment.java
@@ -4,16 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+
 public class HomeFragment extends Fragment {
-
-
+    private ViewFlipper viewFlipper;
+    private ImageButton next,prev;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container,false);
+        View v= inflater.inflate(R.layout.fragment_home, container,false);
+        viewFlipper =v.findViewById(R.id.simpleViewFlipper);
+        next=v.findViewById(R.id.next);
+        prev=v.findViewById(R.id.prev);
+        Animation in = AnimationUtils.loadAnimation(getActivity(), android.R.anim.slide_in_left);
+        Animation out = AnimationUtils.loadAnimation(getActivity(), android.R.anim.slide_out_right);
+        viewFlipper.setInAnimation(in);
+        viewFlipper.setOutAnimation(out);
+
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewFlipper.showNext();
+            }
+        });
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewFlipper.showPrevious();
+            }
+        });
+        return v;
     }
+
 
 }
