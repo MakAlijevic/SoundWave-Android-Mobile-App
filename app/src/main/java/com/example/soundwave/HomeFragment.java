7956d5912860d0d08716a2d01618cd9af9d01da9
emina <emina.sirbubalo@stu.ibu.edu.ba>
2022-06-10 16:40:49 +0200

    added logo, and listview on homepage

--- app/src/main/java/com/example/soundwave/HomeFragment.java
+++ app/src/main/java/com/example/soundwave/HomeFragment.java
@@ -7,8 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -16,11 +17,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+
 
 public class HomeFragment extends Fragment {
     private ViewFlipper viewFlipper;
     private TextView welcomeUsername,songName1,songName2,songName3;
     private ImageButton next,prev,playButton1, playButton2,playButton3;
+    private ListView listView;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v= inflater.inflate(R.layout.fragment_home, container,false);
         viewFlipper =v.findViewById(R.id.simpleViewFlipper);
@@ -31,14 +35,30 @@ public class HomeFragment extends Fragment {
         prev=v.findViewById(R.id.prev);
         playButton1=v.findViewById(R.id.playButton1);
         playButton2=v.findViewById(R.id.playButton2);
-        playButton3=v.findViewById(R.id.playButton3);
+        playButton3=v.findViewById(R.id.playSong);
         Animation in = AnimationUtils.loadAnimation(getActivity(), android.R.anim.slide_in_left);
         Animation out = AnimationUtils.loadAnimation(getActivity(), android.R.anim.slide_out_right);
         viewFlipper.setInAnimation(in);
         viewFlipper.setOutAnimation(out);
         songName1 = v.findViewById(R.id.title1);
         songName2 = v.findViewById(R.id.title2);
-        songName3 = v.findViewById(R.id.title3);
+        songName3 = v.findViewById(R.id.SongName);
+        listView=v.findViewById(R.id.listview);
+        List<Song> songs = SoundWaveDatabase.getInstance(getContext()).songDao().getFiveSongs();
+        Adapter adapter = new Adapter(getContext(), songs);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Song song=(Song) adapterView.getItemAtPosition(i);
+                String title=song.getTitle();
+                Intent intent = new Intent(getContext(), MusicPlayerActivity.class);
+                intent.putExtra("title", title);
+                startActivity(intent);
+            }
+        });
+
 
         playButton1.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/soundwave/IntroActivity.java
+++ app/src/main/java/com/example/soundwave/IntroActivity.java
@@ -7,6 +7,7 @@ import android.content.res.AssetFileDescriptor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.List;
@@ -15,50 +16,39 @@ public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SongDao songDao=SoundWaveDatabase.getInstance(this).songDao();
+        SongDao songDao = SoundWaveDatabase.getInstance(this).songDao();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-
+        Song song = songDao.getSong("Charlie Puth - Light Switch.mp3");
+        if (song != null) {
+            songDao.deleteAll();
+            Toast toast= Toast.makeText(this,"Izbriso",Toast.LENGTH_LONG);
+            toast.show();
+        }
         loadSongs();
 
-
     }
-    public void SignUpButton(View V)
-    {
-        Intent registrationIntent = new Intent(this,RegistrationActivity.class);
+
+    public void SignUpButton(View V) {
+        Intent registrationIntent = new Intent(this, RegistrationActivity.class);
         startActivity(registrationIntent);
     }
 
-    public void LoginButton(View V)
-    {
-        Intent loginIntent = new Intent(this,LoginActivity.class);
+    public void LoginButton(View V) {
+        Intent loginIntent = new Intent(this, LoginActivity.class);
         startActivity(loginIntent);
     }
 
-    public void loadSongs(){
-        SongDao songDao=SoundWaveDatabase.getInstance(this).songDao();
-        Song song1=new Song("Charlie Puth - Light Switch.mp3","03:03");
+    public void loadSongs() {
+        SongDao songDao = SoundWaveDatabase.getInstance(this).songDao();
+        Song song1 = new Song("Charlie Puth - Light Switch.mp3", "Charlie Puth", "03:40", "Light Switch");
         songDao.addSong(song1);
 
-        Song song2=new Song("Charlie Puth - We Dont Talk Anymore.mp3","03:03");
+        Song song2 = new Song("Charlie Puth - We Dont Talk Anymore.mp3", "Charlie Puth", "03:03", "We Dont Talk Anymore");
         songDao.addSong(song2);
 
-        Song song3=new Song("Coldplay X BTS - My Universe.mp3","03:03");
+        Song song3 = new Song("Coldplay X BTS - My Universe.mp3", "Coldplay", "03:03", "My Universe");
         songDao.addSong(song3);
 
-        Song song4=new Song("Dove Cameron - Boyfriend.mp3","03:03");
-        songDao.addSong(song4);
-
-        Song song5=new Song("Dua Lipa - Levitating.mp3","03:03");
-        songDao.addSong(song5);
-
-        Song song6=new Song("Dua Lipa - New Rules.mp3","03:03");
-        songDao.addSong(song6);
-
-        Song song7=new Song("Ed Sheeran - Bad Habits.mp3","03:03");
-        songDao.addSong(song7);
-
-        Song song8=new Song("Ed Sheeran - Shivers.mp3","03:03");
-        songDao.addSong(song8);
     }
 }
